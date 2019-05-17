49fde0ae7b427d078af9fffecc79468bb0d91c93
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-17 15:36:19 -0500

    added TODOs to each class

---
+++ src/assignAttacks.java
@@ -0,0 +1,8 @@
+// TODO: 2019-05-17
+// TODO: Assign attacks to each Pokemon if we can't do this we can assign damage range for each attack and display a random attack.
+
+public class assignAttacks {
+
+}
+
+
---
+++ src/attacks.java
@@ -0,0 +1,6 @@
+// TODO: 2019-05-17
+// TODO: Create attacks (attacks String name; Int Damage;)
+
+public class attacks {
+
+}
