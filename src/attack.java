c862b8bc2ce809144f4b1303e89a63f9cf152fcb
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-17 16:07:27 -0500

    finished attack class functionality

--- src/attack.java
+++ src/attack.java
@@ -2,5 +2,16 @@
 // TODO: Create attack (attack String name; Int Damage;)
 
 public class attack {
+    private long id;
+    private String name;
+    private int damage;
 
+    public attack(long attackId, String attackName,  int attackDamage,){
+        this.id = attackId;
+        this.name = attackName;
+        this.damage = attackDamage;
+    }
+    public String toString(){
+        return String.format("This Pokemon used %s attack and caused %s damage", this.name, ths.damage);
+    }
 }
