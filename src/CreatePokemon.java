4000110d05fcb708b3496e712d3c1becdfa26694
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-05-20 16:38:12 -0500

    removed the Pokemon lvls

--- src/CreatePokemon.java
+++ src/CreatePokemon.java
@@ -3,28 +3,28 @@ public class CreatePokemon {
     private static Pokemon[] pokemons = new Pokemon[18];
 
     public static void main(String[] args) {
-        pokemons[0] = createAPokemon(1, "Pikachu", "electric", 35,1);
-        pokemons[1] = createAPokemon(2, "Charmander", "fire", 39, 1);
-        pokemons[2] = createAPokemon(3, "Squirtle", "water", 44,1);
-        pokemons[3] = createAPokemon(4, "Geodude", "rock",40, 1);
-        pokemons[4] = createAPokemon(5, "Rhyhorn", "rock", 80, 1);
-        pokemons[5] = createAPokemon(6, "Onix", "rock", 35, 1);
-        pokemons[6] = createAPokemon(7, "Mankey", "fighting", 40,1);
-        pokemons[7] = createAPokemon(8, "Machop","fighting", 70, 1);
-        pokemons[8] = createAPokemon(9, "Hitmonchan", "fighting", 50,1);
-        pokemons[9] = createAPokemon(10, "Poliwag", "water",40,1);
-        pokemons[10] = createAPokemon(11, "Staryu", "water", 30,1);
-        pokemons[11] = createAPokemon(12, "Gyarados", "water", 95, 1);
-        pokemons[12] = createAPokemon(13, "Vulpix", "fire", 38,1);
-        pokemons[13] = createAPokemon(14, "Growlith", "fire", 55, 1);
-        pokemons[14] = createAPokemon(15, "Ponyta", "fire", 50, 1);
-        pokemons[15] = createAPokemon(15, "Jolteon", "electric", 65,1);
-        pokemons[16] = createAPokemon(16, "Zapdos", "electric", 90, 1);
-        pokemons[17] = createAPokemon(17, "Voltorb", "electric", 40,1);
+        pokemons[0] = createAPokemon(1, "Pikachu", "electric", 3);
+        pokemons[1] = createAPokemon(2, "Charmander", "fire", 39);
+        pokemons[2] = createAPokemon(3, "Squirtle", "water", 4);
+        pokemons[3] = createAPokemon(4, "Geodude", "rock",40);
+        pokemons[4] = createAPokemon(5, "Rhyhorn", "rock", 80);
+        pokemons[5] = createAPokemon(6, "Onix", "rock", 35);
+        pokemons[6] = createAPokemon(7, "Mankey", "fighting", 4);
+        pokemons[7] = createAPokemon(8, "Machop","fighting", 70);
+        pokemons[8] = createAPokemon(9, "Hitmonchan", "fighting", 5);
+        pokemons[9] = createAPokemon(10, "Poliwag", "water",4);
+        pokemons[10] = createAPokemon(11, "Staryu", "water", 3);
+        pokemons[11] = createAPokemon(12, "Gyarados", "water", 95);
+        pokemons[12] = createAPokemon(13, "Vulpix", "fire", 3);
+        pokemons[13] = createAPokemon(14, "Growlith", "fire", 55);
+        pokemons[14] = createAPokemon(15, "Ponyta", "fire", 50);
+        pokemons[15] = createAPokemon(15, "Jolteon", "electric", 6);
+        pokemons[16] = createAPokemon(16, "Zapdos", "electric", 90);
+        pokemons[17] = createAPokemon(17, "Voltorb", "electric", 4);
         list(pokemons);
     }
-    private static Pokemon createAPokemon(int id, String name, String type, int hp, int lvl){
-        Pokemon pokemon = new Pokemon(id, name, type, hp, lvl);
+    private static Pokemon createAPokemon(int id, String name, String type, int hp){
+        Pokemon pokemon = new Pokemon(id, name, type, hp);
         return pokemon;
     }
 
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,18 +4,16 @@ public class Pokemon {
     private String name;
     private String type;
     private int hp;
-    private int lvl;
 
-    public Pokemon(long pokemonId, String pokemonName, String pokemonType, int pokemonHp, int pokemonLvl){
+    public Pokemon(long pokemonId, String pokemonName, String pokemonType, int pokemonHp){
     this.id = pokemonId;
     this.name = pokemonName;
     this.type = pokemonType;
     this.hp = pokemonHp;
-    this.lvl = pokemonLvl;
     }
 
     public String toString(){
-        return String.format(" id: %s \n Pokemon: %s \n Type: %s \n Current hp: %s \n Current lvl: %s \n",this.id, this.name, this.type, this.hp, this.lvl);
+        return String.format(" id: %s \n Pokemon: %s \n Type: %s \n Current hp: %s \n",this.id, this.name, this.type, this.hp);
     }
 
 //    public long getId() {
@@ -34,15 +32,9 @@ public class Pokemon {
 //        return hp;
 //    }
 //
-//    public int getLvl() {
-//        return lvl;
-//    }
 //
 //    public void setHp(int hp) {
 //        this.hp = hp;
 //    }
 //
-//    public void setLvl(int lvl) {
-//        this.lvl = lvl;
-//    }
 }
